68801fd8494b399c4e87d835e24992775ca4630c
Oleg Lialin <lyalin.pa1nt3r@gmail.com>
2025-06-06 15:20:54 +0400

    bp

--- Pages/BattlePlanner.cshtml.cs
+++ Pages/BattlePlanner.cshtml.cs
@@ -1,24 +1,39 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Tournoi.Battles;
-
+using Tournoi.DB;
 namespace Tournoi.Pages
 {
-    public class BattlePlanerModel : PageModel
+    public class ViewSchemesModel : PageModel
     {
-        private readonly BattlePlanner _planner;
+        private readonly AppDBContext _context;
 
-        public BattlePlanerModel(BattlePlanner planner)
+        public List<List<string>> Battles { get; set; } = new();
+        public List<string> SelectedPlayers { get; set; } = new();
+
+        public ViewSchemesModel(AppDBContext context)
         {
-            _planner = planner;
+            _context = context;
         }
 
         public void OnGet()
         {
-            string[] names = ["À", "Á", "Â", "Ã", "Ä", "Å"];
-            string[][] battles = _planner.ApplyScheme("six", names);
-            foreach (string[] battle in battles)
+            var players = _context.Players
+                .Include(p => p.Person)
+                .Where(p => p.SI)
+                .OrderBy(p => EF.Functions.Random())
+                .Take(7)
+                .Select(p => p.Person.FullName)
+                .ToList();
+
+            SelectedPlayers = players;
+
+            if (players.Count == 7)
             {
-                Console.WriteLine(string.Join("\t", battle));
+                var planner = new BattlePlanner();
+                var scheme = planner.ApplyScheme("seven", players.ToArray());
+
+                Battles = scheme.Select(row => row.ToList()).ToList();
             }
         }
     }
